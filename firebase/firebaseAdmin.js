099b6e749195eceefeb9b9ebc229bbf9e23d36fd
Sunil Babu <sakibandasunilbabu@gmail.com>
2026-01-20 20:29:49 +0530

    Fix firebase env varibales names

---
+++ firebase/firebaseAdmin.js
@@ -0,0 +1,9 @@
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.project_id,
+    clientEmail: process.env.client_email,
+    privateKey: process.env.private_key.replace(/\\n/g, '\n'),
+  }),
+});
