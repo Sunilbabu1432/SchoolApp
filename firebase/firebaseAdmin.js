0cd7b8e668e077ae108f1f1b5d6d3f4d7a34fcfd
Sunil Babu <sakibandasunilbabu@gmail.com>
2026-01-20 21:04:32 +0530

    Fix Firebase Admin init using single env Json

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,9 +1,17 @@
 const admin = require('firebase-admin');
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.project_id,
-    clientEmail: process.env.client_email,
-    privateKey: process.env.private_key.replace(/\\n/g, '\n'),
-  }),
-});
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT env variable missing');
+}
+
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+  );
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+module.exports = admin;
