99e5559e59894620e97d9cd5eea429ef19bb89d9
Sunil Babu <sakibandasunilbabu@gmail.com>
2026-01-21 23:47:40 +0530

    Firebase and notification updated

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,16 +1,18 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    Buffer.from(
-      process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
-      'base64'
-    ).toString('utf8')
-  );
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
   });
+
+  console.log(
+    '🔥 Firebase Admin initialized for project',
+    process.env.FIREBASE_PROJECT_ID
+  );
 }
 
 module.exports = admin;
